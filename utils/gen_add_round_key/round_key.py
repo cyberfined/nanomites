ed2f86909a0c85e77a5b26283c9cf2d90dfecc6c
cyberfined <cyberfined@protonmail.com>
2023-10-12 21:27:42 +0300

    Remove useless utils

--- utils/gen_add_round_key/round_key.py
+++
@@ -1,9 +0,0 @@
-class RoundKey:
-    def __init__(self, roundKey, address):
-        self.roundKey = roundKey
-        self.address = address
-
-    def getNum(self, rnd, j):
-        start = 16*rnd + j*4
-        bts = self.roundKey[start:start+4]
-        return (bts[3] << 24) | (bts[2] << 16) | (bts[1] << 8) | bts[0]
